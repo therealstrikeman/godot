7099c4f16ab31e628d9cbfaacc89e4fcb92ef81b
ole <olevsmortensen@dbc5radio.dk>
2025-04-16 20:54:57 +0200

    add timer counterdown from 30 seconds

--- scripts/score_label.gd
+++ scripts/score_label.gd
@@ -1,16 +1,26 @@
 extends Label
- 
-var score: int = 0 
-var player_win:bool = false
+
+var score: int = 0
+var time_left: int = 30
+var player_win: bool = false
 
 func _ready():
+	$GameTimer.wait_time = 1.0  # 1 second intervals
+	$GameTimer.timeout.connect(_on_GameTimer_timeout)
+	$GameTimer.start()
 	update_score_display()
-	
-	
-func update_score_display():
-	text = "Score: %d" % score
 
+func update_score_display():
+	text = "Score: %d\nTime: %d" % [score, time_left]
 
 func _on_target_hit():
 	score += 1
 	update_score_display()
+
+func _on_GameTimer_timeout():
+	time_left -= 1
+	update_score_display()
+	
+	if time_left <= 0:
+		$GameTimer.stop()
+		text += "\nTime's up!"
