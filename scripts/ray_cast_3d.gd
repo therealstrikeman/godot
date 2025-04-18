501ee324aa25bd02e06e3b6be7e0e8adab9d272b
Ole Vølund Skov Mortensen <22987563+OleMortensen8@users.noreply.github.com>
2025-04-18 02:40:24 +0200

    Revert "add timer counterdown from 30 seconds"

--- scripts/ray_cast_3d.gd
+++ scripts/ray_cast_3d.gd
@@ -5,7 +5,7 @@ extends RayCast3D
 
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 
--- scripts/score_label.gd
+++ scripts/score_label.gd
@@ -1,25 +1,16 @@
 extends Label
-
-var score: int = 0
-var time_elapsed: int = 0
-var player_win: bool = false
+ 
+var score: int = 0 
+var player_win:bool = false
 
 func _ready():
-	$GameTimer.wait_time = 1.0  # 1 second intervals
-	$GameTimer.timeout.connect(_on_GameTimer_timeout)
-	$GameTimer.start()
 	update_score_display()
-
+	
+	
 func update_score_display():
-	var minutes = time_elapsed / 60
-	var seconds = time_elapsed % 60
-	var time_str = "%02d:%02d" % [minutes, seconds]
-	text = "Score: %d\nTime: %s" % [score, time_str]
+	text = "Score: %d" % score
+
 
 func _on_target_hit():
 	score += 1
-	update_score_display()
-
-func _on_GameTimer_timeout():
-	time_elapsed += 1
 	update_score_display()
